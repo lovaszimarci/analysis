1e763babc645530a2617cf73ce3b52b4e5338d96
Lovászi Márton <lovaszimarci@gmail.com>
2022-10-07 21:08:09 +0200

    matrix

---
+++ meth.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+
+
+
+
+
+
+int main()
+{
+    //graph axis base creation 
+    char** graph = new char*[50];   // 50 char in heap
+    for(int i = 0; i < 50; i++){
+        graph[i] = new char[50];    // for every char in heap plus 50 char
+    }
+
+
+
+
+}
