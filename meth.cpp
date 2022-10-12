46b4863bb0ba8b8f9833f2d813348bafa5cf1413
Lovászi Márton <lovaszimarci@gmail.com>
2022-10-12 22:58:24 +0200

    j

--- meth.cpp
+++ meth.cpp
@@ -4,7 +4,7 @@
 #include <map>
 #include <cmath>
  
-int value_of_base_x_cordinates[51] = {-25,-24,-23,-22,-21,-20,-19,-18,-17,-16
+int value_of_base_x_cordinates[50] = {-25,-24,-23,-22,-21,-20,-19,-18,-17,-16
 ,-15,-14,-13,-12,-11,-10,-9,-8,-7,-6,-5-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
  
 void modification()
@@ -42,6 +42,24 @@ void modification()
                i = i - modification_number_int;
            }
        }
+
+       else if(input == "3"){
+            std::cout<< "how much do you want to multiply?"<<std::endl;
+            std::cin>> modification_number_string;
+            int modification_number_int = stoi(modification_number_string);
+            for(auto& i : value_of_base_x_cordinates){
+                i = i * modification_number_int;
+            }
+       }
+
+       else if(input == "4"){
+        std::cout<< "how much do you want to divide with?"<<std::endl;
+           std::cin>> modification_number_string;
+           int modification_number_int = stoi(modification_number_string);
+           for(auto& i : value_of_base_x_cordinates){
+               i = i / modification_number_int;
+           }
+       }
  
  
       
@@ -55,7 +73,7 @@ int main()
    std::map<int, int> kordinatak;
    modification();
    for(auto & i : value_of_base_x_cordinates){
-       std::cout<< i << " ";
+       std::cout<< i << std::endl;
    }
 }
  
